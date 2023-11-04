0caa79f7255881c0b3b58d926f68132279363288
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2023-11-04 17:07:47 -0600

    test

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,8 +2,8 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject, catchError, map, tap, throwError } from 'rxjs';
 // import { environment } from 'src/environments/environment.prod';
-// import { environment } from '../../../environment.prod';
-import { environment } from '/etc/secrets/environment.prod';
+import { environment } from '../../../environment.prod';
+// import { environment } from '/etc/secrets/environment.prod';
 import { User } from './auth/user.model';
 import { Router } from '@angular/router';
 
