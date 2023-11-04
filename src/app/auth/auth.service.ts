44744ffe0a448dfa8604ca007ba8cc947d53b8b5
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2023-11-04 16:57:35 -0600

    update path

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject, catchError, map, tap, throwError } from 'rxjs';
 // import { environment } from 'src/environments/environment.prod';
-import { environment } from 'environment.prod';
+import { environment } from '../../../environment.prod';
 import { User } from './auth/user.model';
 import { Router } from '@angular/router';
 
